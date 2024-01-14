236cc500824246aac02de97d0a88156cd14bfa6b
Kukesan <kkukesan@gmail.com>
2024-01-14 16:49:47 +0530

    Add project files.

---
+++ Tlauom_2024/Entities/TLAContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tlauom_2024.Entities.Models;
+
+namespace Tlauom_2024.Entities
+{
+    public class TLAContext : DbContext
+    {
+            public TLAContext(DbContextOptions<TLAContext> options)
+                : base(options)
+        {
+
+            }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<School> Schools { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Apply configurations
+        }
+    }
+}
